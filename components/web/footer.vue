88357e26667b8ed27a0e0bee85b8423029265574
Achbar Wahyudhi <achbarwahyudhi2001@gmail.com>
2024-03-27 22:59:08 +0700

    bismillah

---
+++ components/web/footer.vue
@@ -0,0 +1,56 @@
+<template>
+  <footer class="pt-5" style="background: rgb(255, 255, 255); border-top: 5px solid #1B1A55">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-5 mb-4">
+          <h4 class="font-weight-bold">TENTANG</h4>
+          <hr style="border-top: 3px solid rgb(226, 232, 240); border-radius: 0.5rem;">
+          <!-- <p> 
+              Toko Mira Official Terpercaya di Indonesia. Jual Beli Aman &amp; Harga Termurah! Belanja
+              sekarang. </p> -->
+          <p style="font-weight: bold;">Temukan Pengalaman Belanja Online Terbaik di E-Commerce Toko Mira!</p>
+          <p>
+            Selamat datang di Toko Mira, destinasi utama Anda untuk tren terbaru dalam dunia fashion. Kami bangga
+            menyajikan koleksi pakaian berkualitas tinggi untuk semua gaya.
+          </p>
+        </div>
+        <div class="col-md-3 mb-4">
+          <!-- <h4 class="font-weight-bold">METODE PEMBAYARAN</h4> -->
+          <h4 class="font-weight-bold">KONTAK</h4>
+          <hr style="border-top: 3px solid rgb(226, 232, 240); border-radius: 0.5rem;">
+          <div class="row">
+            <ul class="list-inline ml-3">
+              <li class="list-inline-item"><a href="#"><i class="fab fa-facebook"></i> Facebook</a></li><br>
+              <li class="list-inline-item mt-2"><a href="#"><i class="fab fa-twitter"></i> Twitter</a></li><br>
+              <li class="list-inline-item mt-2"><a href="https://www.instagram.com/mstoresolo?igsh=M3Q3dDRyNDRnbXc1"><i
+                    class="fab fa-instagram"></i> Instagram</a></li><br>
+              <li class="list-inline-item mt-2"><a
+                  href="whatsapp://send?text=Hallo Saya Ingin Bertanya?&phone=+6285602622966"><i
+                    class="fab fa-whatsapp"></i> +62 85602622966</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <h4 class="font-weight-bold">JAM OPERASIONAL</h4>
+          <hr style="border-top: 3px solid rgb(226, 232, 240); border-radius: 0.5rem;">
+          <p><i class="fa fa-clock"></i> Toko Buka Setiap Hari : <br><br> <strong>Senin - Sabtu</strong> ( 09.00 -
+            16.00 ) <br> <strong>Minggu</strong> ( 09.00 - 14.00 ) </p>
+        </div>
+      </div>
+      <div class="row text-center mt-3 pb-3">
+        <div class="col-md-12">
+          <hr> Copyright © 2024 E-Commerce Toko Mira. All rights reserved.
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
---
+++ layouts/admin.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="c-app">
+        <div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
+            <div class="c-sidebar-brand d-lg-down-none">
+                <img src="/images/logomira.jpg" class="img-logoadmin" width="60px"> 
+                <span class="ml-2 font-weight-bold">ADMIN TOKO MIRA</span>
+            </div>
+
+            <!-- sidebar -->
+            <Sidebar />
+            <!-- end sidebar -->
+
+        </div>
+        <div class="c-wrapper c-fixed-components">
+
+            <!-- header -->
+            <Header />
+            <!-- end header -->
+
+            <div class="c-body">
+
+                <!-- content -->
+                <Nuxt />
+                <!-- end content -->
+
+                <footer class="c-footer">
+                    Copyright © 2024 E-Commerce Toko Mira. All rights reserved.
+                </footer>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import Header from '@/components/admin/header.vue'
+import Sidebar from '@/components/admin/sidebar.vue'
+
+export default {
+
+    //middleware
+    middleware: 'isAdmin',
+
+    //register components
+    components: {
+        Header,
+        Sidebar
+    }
+
+}
+</script>
+
+<style></style>
