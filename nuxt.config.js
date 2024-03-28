9216fca9c055db41db7a156e75db87fdf95fb29c
achbar2001 <112878636+achbar2001@users.noreply.github.com>
2024-03-28 14:24:29 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -185,7 +185,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://localhost:8000'
+    baseURL: 'http://achbar.my.id'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
