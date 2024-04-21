8335078d6abbe71e00e423c335b5d41b8c2ecb1a
Achbar Wahyudhi <achbarwahyudhi2001@gmail.com>
2024-04-21 18:54:39 +0700

    update new

--- nuxt.config.js
+++ nuxt.config.js
@@ -185,7 +185,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://localhost:8000'
+    baseURL: 'https://achbar.my.id/'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
